1b4f06b90234d620df4c4a99804bdca8c6a88e63
Henry Crofts <hwcrofts@gmail.com>
2018-11-12 15:36:12 +0000

    Started player scripts / Imported unity standard assets and water shader
    -Made a start on playerStates
    ""                   "" playerController
    -Imported the unity standard assets for the water shader to work
    - Imported free water shader (requires standard assets)

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerStates.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour
+{
+    private enum PlayerState { pCannonball, pGunpowder, pMop, pBucket, pWood, pTorch, pEmpty, pHoldingOn };
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerState playerState;
+
+        playerState = PlayerState.pEmpty;
+	}
+}
+

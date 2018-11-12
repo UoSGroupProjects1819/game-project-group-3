c1f83777fdec85c99cf85d8c8eac64bec4036509
Henry Crofts <hwcrofts@gmail.com>
2018-11-12 16:08:45 +0000

    Updated playerController

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerController.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 5;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Movement();
+
+
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "Mop")
+        {
+            Debug.Log("Found the mop");
+        }
+    }
+
+    void Interact()
+    {
+        
+    }
 }

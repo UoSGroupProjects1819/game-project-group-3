1c9c3a5a7f262907d39622845047f418680179cb
Henry Crofts <hwcrofts@gmail.com>
2018-11-13 16:55:13 +0000

    Added water shader, removed unity standard assets

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Interactables/Mop/Mop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mop : Interactable
+{
+    public override void Action(GameObject player)
+    {
+        Debug.Log("Mop");
+    }
+}

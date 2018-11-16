77534234448e33af9962944552187b2371b4c4be
Tom <tom.gibbs@hotmail.co.uk>
2018-11-16 22:58:35 +0000

    Player scripts updated to accept XboxOne controller input

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerController.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerController.cs
@@ -7,24 +7,49 @@ public class PlayerController : MonoBehaviour {
 
     public PlayerStates playerState;
 
+    [Header("[Mapped Controls]")]
+    public string Abutton = "A_P1";
+    public string Bbutton = "B_P1";
+    public string DpadHorizontal = "Dpad_Left/Right_P1";
+    public string DpadVertical = "Dpad_Up/Down_P1";
 
 
-    
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         playerState = this.GetComponent<PlayerStates>();  
     }
 
     private void Update()
     {
         DropItem();
+
+
+        //testing Dpad
+        if (Input.GetAxisRaw(DpadVertical) > 0)
+        {
+            Debug.Log("up dpad");
+        }
+        else if (Input.GetAxisRaw(DpadVertical) < 0)
+        {
+            Debug.Log("down dpad");
+        }
+
+        if (Input.GetAxisRaw(DpadHorizontal) > 0)
+        {
+            Debug.Log("right dpad");
+        }
+        else if (Input.GetAxisRaw(DpadHorizontal) < 0)
+        {
+            Debug.Log("left dpad");
+        }
     }
 
 
     void FixedUpdate()
     {
-        
+
     }
 
     private void OnTriggerStay(Collider col)
@@ -35,7 +60,7 @@ public class PlayerController : MonoBehaviour {
         if (other != null)
         {
             Debug.Log("Hit interactable");
-            if (Input.GetKey(KeyCode.I))
+            if (Input.GetKey(KeyCode.I) || Input.GetButtonDown(Abutton))
             {
                 Debug.Log("Action button pressed");
                 other.Action(this.gameObject);
@@ -48,7 +73,7 @@ public class PlayerController : MonoBehaviour {
         if(playerState.itemHeld == null)
         { return; }
 
-        if (playerState.itemHeld != null && Input.GetKey(KeyCode.U))
+        if (playerState.itemHeld != null && Input.GetKey(KeyCode.U) || Input.GetButtonDown(Bbutton))
         {
             Interactable other = this.GetComponentInChildren<Interactable>();
 
--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerMovement.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    [Header("[Mapped Controls]")]
+    public string horizontalInput = "Horizontal_P1";
+    public string verticalInput = "Vertical_P1";
+
     private Rigidbody rb;
 
+    [Header("[Movement variables]")]
     public float speed = 5f;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         rb = this.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        float moveHorizontal = -Input.GetAxis("Horizontal");
-        float moveVertical = -Input.GetAxis("Vertical");
+        float moveHorizontal = -Input.GetAxis(horizontalInput);
+        float moveVertical = -Input.GetAxis(verticalInput);
 
 
         Vector3 move = new Vector3(moveHorizontal * speed, 0, moveVertical * speed);

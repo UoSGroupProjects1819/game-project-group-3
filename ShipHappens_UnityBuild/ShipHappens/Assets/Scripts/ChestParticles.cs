593317b8ae4e78563a9263c8c577d859b03592d8
Tom <tom.gibbs@hotmail.co.uk>
2018-11-30 06:06:05 +0000

    Treasure island timeline & particle system updates

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/ChestParticles.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/ChestParticles.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestParticles : MonoBehaviour
 {
     public ParticleSystem chestPS;
+    public ParticleSystem rewardPS;
+    public SpriteRenderer rewardSprite;
+
+    private void Start()
+    {
+        rewardSprite.enabled = false;
+    }
 
     public void ChestParticlePlay()
     {
         chestPS.Play();
+    }
+
+    public void RewardParticlePlay()
+    {
+        rewardSprite.enabled = true;
+        rewardPS.Play();
     }
 }

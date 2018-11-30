db9d646371242a5e39b746ec597801e71691480d
Tom <tom.gibbs@hotmail.co.uk>
2018-11-30 04:57:14 +0000

    Treasure Island reward sequence and particle system

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Camera/CameraFocus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+    public GameObject chest;
+
+
+	void LateUpdate ()
+    {
+        transform.LookAt(chest.transform);
+	}
+}

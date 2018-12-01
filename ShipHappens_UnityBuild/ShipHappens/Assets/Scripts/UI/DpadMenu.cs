9937e73112b3d6e773cecb3f7a91d8ceb7bdccc4
Tom <tom.gibbs@hotmail.co.uk>
2018-12-01 13:55:42 +0000

    UI Dpad menu update

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/DpadMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DpadMenu : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public RawImage dpad;
+    //public Image woodImg;
+    //public Image cannonballImg;
+    //public Image gunpowderImg;
+
+    public int menuCount;
+    public bool inMenu;
+
+    Color dpadSolid;
+    Color dpadFaint;
+    Color dpadCol;
+
+
+	void Start ()
+    {
+        dpadSolid = dpad.color;
+        dpadSolid.a = 0.95f;
+
+        dpadFaint = dpad.color;
+        dpadFaint.a = 0.55f;
+    }
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            menuCount++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            menuCount--;
+        }
+    }
+
+    private void Update()
+    {
+        if (menuCount >= 1)
+        {
+            inMenu = true;
+            canvasGroup.alpha = Mathf.Lerp(0.95f, 0.55f, 0.1f);
+        }
+        else
+        {
+            inMenu = false;
+            canvasGroup.alpha = Mathf.Lerp(0.55f, 0.95f, 0.1f);
+        }
+
+        Debug.Log("dpad alpha value: " + dpad.color.a);
+    }
+}
---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/WoodTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WoodTimer : MonoBehaviour
+{
+    public Image countdownImg;
+    public float fillCountdown = 1f;
+    public float fillSpeed;
+
+    public bool onCooldown;
+
+    private void Start()
+    {
+        countdownImg.fillAmount = 0f;
+    }
+
+    void Update()
+    {
+        if (onCooldown)
+        {
+            fillCountdown = fillCountdown - fillSpeed * Time.deltaTime;
+            countdownImg.fillAmount = fillCountdown;
+
+            if (fillCountdown <= 0)
+            {
+                onCooldown = false;
+            }
+        }
+    }
+}

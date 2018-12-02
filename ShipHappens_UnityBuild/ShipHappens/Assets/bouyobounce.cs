c9dbbf3b66b7f22cc369e01cdaed167fb50b0c82
Tom <tom.gibbs@hotmail.co.uk>
2018-12-02 15:14:07 +0000

    Bouyancy test scene

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/bouyobounce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouyobounce : MonoBehaviour
+{
+    public GameObject[] things;
+
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            for (int i = 0; i < things.Length; i++)
+            {
+                float x = Random.RandomRange(-1f, 1f);
+                float y = Random.RandomRange(0.2f, 1.2f);
+                float z = Random.RandomRange(0.1f, 0.4f);
+
+                things[i].GetComponent<Rigidbody>().AddForce(new Vector3(x, y, z), ForceMode.Impulse);
+            }
+        }
+    }
+}

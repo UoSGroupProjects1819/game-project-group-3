fdd213e9be004c31790db8b11cde848cd71bb7e3
Tom <tom.gibbs@hotmail.co.uk>
2018-12-02 02:53:33 +0000

    Bouyancy test improvements

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Managers/FloodController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloodController : MonoBehaviour
+{
+    public GameObject floodPlane;
+    public float targetHeight;
+    public float currentHeight;
+    public float floodSpeed;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}

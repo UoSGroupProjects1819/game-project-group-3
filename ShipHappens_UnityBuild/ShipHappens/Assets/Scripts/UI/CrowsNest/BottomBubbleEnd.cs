b626a96e7d7845f106a63b800bcae3342243aa74
Tom <tom.gibbs@hotmail.co.uk>
2018-12-09 18:40:37 +0000

    Presentation 3 updates

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/CrowsNest/BottomBubbleEnd.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/CrowsNest/BottomBubbleEnd.cs
@@ -8,6 +8,9 @@ public class BottomBubbleEnd : MonoBehaviour
     private Animator bottomAnimator;
     public CrowsNestUI CNui;
 
+    public ScreenShake shake;
+    public GameObject water;
+
     void Start ()
     {
         bottomAnimator = GetComponent<Animator>();
@@ -19,4 +22,32 @@ public class BottomBubbleEnd : MonoBehaviour
         CNui.playBottom = false;
         bottomAnimator.SetBool("PlayBottom", false);
     }
+
+    //void MoveWater()
+    //{
+    //    shake.mediumShake = true;
+    //    shake.shouldShake = true;
+    //    Vector3 currentWaterPos = water.transform.position;
+    //    Vector3 nextWaterPos;
+    //    nextWaterPos.x = currentWaterPos.x;
+    //    nextWaterPos.z = currentWaterPos.z;
+    //    nextWaterPos.y = 10f;
+
+    //    water.transform.position = nextWaterPos;
+    //}
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyUp(KeyCode.B))
+    //    {
+    //        Debug.Log("ye");
+    //        Vector3 currentWaterPos = water.transform.position;
+    //        Vector3 nextWaterPos;
+    //        nextWaterPos.x = currentWaterPos.x;
+    //        nextWaterPos.z = currentWaterPos.z;
+    //        nextWaterPos.y = currentWaterPos.y - 0.25f;
+
+    //        water.transform.position = nextWaterPos;
+    //    }
+    //}
 }

a36bf167e086e41030634568edeaf0aad45e2ca0
Henry Crofts <hwcrofts@gmail.com>
2019-01-17 19:28:08 +0000

    Added enemy movement

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/NonInteractables/Enemy/EnemyMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{ 
+    public bool isLeftSide;
+    public bool isTopSide;
+
+    public float speed;
+
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (isLeftSide)
+        {
+            if (isTopSide)
+            {
+                transform.Translate(Vector3.forward * speed *Time.deltaTime);
+            }
+            else if (!isTopSide)
+            {
+                transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+            }
+        }
+        else if (!isLeftSide)
+        {
+            if (isTopSide)
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+            else if (!isTopSide)
+            {
+                transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public void Spawn(bool isLeft, bool isTop)
+    {
+        isLeftSide = isLeft;
+        isTopSide = isTop;
+    }
+}

977d2fb9f4b248dcf24796c2ab259f369074a132
Henry Crofts <hwcrofts@gmail.com>
2019-01-23 17:44:20 +0000

    Added the event manager script

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public float timer;
+
+    [Header("[Task Reference]")]
+    public static string highestActivityName;
+    public static int highestActivityAmount;
+    public static Dictionary<string, int> activeTasks = new Dictionary<string, int>();
+
+    public static void AddTask(string name)
+    {
+        if (activeTasks.ContainsKey(name))
+        {
+            activeTasks[name]++;
+        }
+        else
+        {
+            activeTasks.Add(name, 1);
+        }
+    }
+
+    public static void RemoveTask(string name)
+    {
+        if (activeTasks.ContainsKey(name))
+        {
+            if (activeTasks[name] <= 1)
+            {
+                activeTasks[name] = 0;
+            }
+            else
+            {
+                activeTasks[name]--;
+            }
+        }
+        else
+        {
+            Debug.LogError("Task Does Not Exist!");
+        }
+    }
+
+    public static void FindMostActive()
+    {
+        highestActivityName = null;
+        highestActivityAmount = 0;
+
+        foreach (KeyValuePair<string, int> task in activeTasks)
+        {
+            if (task.Value > highestActivityAmount)
+            {
+                highestActivityName = task.Key;              
+            }
+        }
+
+        Debug.Log("Highest Activity is: " + highestActivityName);
+    }
+}

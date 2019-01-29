6b3c3b80846fdc4f6c92e1c356d509c650a8a797
Tom <tom.gibbs@hotmail.co.uk>
2019-01-29 17:55:39 +0000

    UI timer bar

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/TimerBars/DistanceTimerBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceTimerBar : MonoBehaviour
+{
+    public float distanceTimer;
+    public float maxTimer;
+
+    public Slider distanceSlider;
+
+    void Start()
+    {
+        distanceTimer = maxTimer;
+    }
+
+
+
+    void Update()
+    {
+        distanceTimer -= Time.deltaTime;
+
+        
+
+
+        float percentageFill = Mathf.InverseLerp(0, maxTimer, distanceTimer);
+        distanceSlider.value = percentageFill;
+        Debug.Log("percentage fill: " + percentageFill);
+    }
+}

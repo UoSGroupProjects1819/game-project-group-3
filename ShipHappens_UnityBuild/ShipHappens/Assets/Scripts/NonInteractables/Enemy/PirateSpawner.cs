ae595aadda6e3d0f480e6381c807e61d5b49398b
Tom <tom.gibbs@hotmail.co.uk>
2019-01-30 17:02:15 +0000

    Updated enemy controller, flag movement
    Co-Authored-By: Henry Crofts <hwcrofts@gmail.com>

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/NonInteractables/Enemy/PirateSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateSpawner : MonoBehaviour
+{
+    public GameObject pirateFlag;
+    public GameObject[] shipSpawners;
+    //public GameObject shipCentre;
+
+    //public bool isBow = false;
+    //public bool isStern = false;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SpawnPirate();
+        }
+    }
+
+
+    void SpawnPirate()
+    {
+        int random = Random.Range(0, 4);
+
+        //if (shipSpawners[random].transform.position.z > shipCentre.transform.position.z) //+ is to rear
+        //{
+        //    isStern = true;
+        //}
+        //else //- is to front
+        //{
+        //    isBow = true;
+        //}
+
+        Instantiate(pirateFlag, shipSpawners[random].transform.position, shipSpawners[random].transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("is colliding");
+        if (other.gameObject.tag == "PirateFlag")
+        {
+            Debug.Log("is pirate");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/LookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public GameObject makeMeLookAt;
+    public GameObject objToFace;
+
+    void Update()
+    {
+        transform.LookAt(objToFace.transform);
+    }
+}

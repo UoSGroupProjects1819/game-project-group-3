ff75d716f9226c3b59d96d698eef49a8560d208d
Tom <tom.gibbs@hotmail.co.uk>
2019-02-01 04:48:47 +0000

    [incomplete] enemy cannon attack logic

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/NonInteractables/Enemy/EnemyAttack.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/NonInteractables/Enemy/EnemyAttack.cs
@@ -6,17 +6,46 @@ public class EnemyAttack : MonoBehaviour
 {
     public GameObject cannonballPrefab;
     public Projector projectorPrefab;
+    public Projector shrinkingProjector;
+    public GameObject holePrefab;
 
 
-    void DropCannonball()
+    private void Update()
     {
-        //pick random point
-
-        //spawn projector
-        //spawn cannonball
+        if (Input.GetMouseButtonUp(1))
+        {
+            SpawnCannonball();
+        }
 
         //shrink projector as cannonball y-axis drops
+        shrinkingProjector.orthographicSize = cannonballPrefab.transform.position.y - 8;
+    }
 
+    private void FixedUpdate()
+    {
+        //
+    }
+
+    void SpawnCannonball()
+    {
+        Vector3 randomPos; //pick random point
+
+        //raycast down from randomPos
+        //if hit ship deck, instantiate cannonball
+        //otherwise reposition (either use one large rect or legit random)
+
+        //spawn prefab
+        //Instantiate(cannonballPrefab, randomPos, Quaternion.identity);
+
+        //once hole has replaced cannonball on impact, give hole a sphere trigger collider that will set it to flip if cannonball enters trigger
+        //...maybe do this on the repaired holes.
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
         //when hit deck swap model to hole
+        Instantiate(holePrefab, cannonballPrefab.transform.position, cannonballPrefab.transform.rotation);
+        Destroy(projectorPrefab);
+        //increment static hole count
     }
 }

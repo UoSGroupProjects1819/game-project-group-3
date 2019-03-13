acd65ac29b2a6d038bfba0188766d719357069a2
Tom <tom.gibbs@hotmail.co.uk>
2019-03-13 04:28:29 +0000

    Potential main menu, early transitions

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/PlayerSelection/WhaleSplashNextLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleSplashNextLevel : MonoBehaviour
+{
+    public ParticleSystem splash;
+    public Animator anim;
+
+    public void PlayWhaleSplash()
+    {
+        splash.Play();
+    }
+
+    public void LoadGameLevel()
+    {
+        anim.SetBool("PlayWhaleSplash", false);
+        //loadgamelevel
+        Debug.Log("load the game");
+    }
+}
---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/PlayerSelection/mainmenucannons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenucannons : MonoBehaviour
+{
+    public ParticleSystem cannonPS;
+
+
+    public void PlayCannonParticles()
+    {
+        cannonPS.Play();
+    }
+}

f1d6bbaa7bad2cdf0f5acac89f90d39031c488ca
Henry Crofts <hwcrofts@gmail.com>
2019-03-30 13:25:40 +0000

    Uploaded meeting minutes

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/New Player/Player.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/Player/New Player/Player.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public enum Direction { up, left, right }
+    Direction direction;
 
+    public PlayerStates playerStates;
+    public PlayerInput playerInput;
+    public PlayerMovement playerMovement;
+
+    private bool upIsPressed, leftIsPressed, rightIsPressed;
+    private bool edge = false;
+    private bool isInteracting = false;
+
+    private void OnValidate()
+    {
+        playerStates = GetComponent<PlayerStates>();
+        playerInput = GetComponent<PlayerInput>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (playerInput.ButtonIsDown(PlayerInput.Button.B))
+        {
+            Debug.Log("Player Dropped item!");
+        }
+    }
 }

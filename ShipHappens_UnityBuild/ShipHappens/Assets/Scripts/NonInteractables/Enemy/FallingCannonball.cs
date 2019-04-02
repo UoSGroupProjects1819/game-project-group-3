4bac3bb58db07fbe911ac95de55b86e9d8ab04b5
Tom <tom.gibbs@hotmail.co.uk>
2019-04-02 23:03:21 +0100

    De-pool held object on cannonball death, instead of drop
    Co-Authored-By: Henry Crofts <hwcrofts@gmail.com>

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/NonInteractables/Enemy/FallingCannonball.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/NonInteractables/Enemy/FallingCannonball.cs
@@ -29,7 +29,7 @@ public class FallingCannonball : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             PlayerStates playerStates = collision.gameObject.GetComponent<PlayerStates>();
-            playerStates.gameObject.GetComponentInChildren<InteractableObjs>().DropItem();
+            playerStates.itemHeld.SetActive(false);
             playerStates.playerState = PlayerStates.PlayerState.pDead;
         }
     }

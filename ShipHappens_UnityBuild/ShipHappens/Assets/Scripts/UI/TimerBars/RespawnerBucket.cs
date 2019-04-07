647ab89820fca5eb86eadc037649d51bdb4a7461
Tom <tom.gibbs@hotmail.co.uk>
2019-04-07 02:58:36 +0100

    Interactable respawner scripts

---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/TimerBars/RespawnerBucket.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RespawnerBucket : MonoBehaviour
+{
+    public GameObject bucket;
+    BucketStates bucketStates;
+    public GameObject spawnPoint;
+    public GameObject sea;
+    private Rigidbody rb;
+
+    public float timer;
+    public float initialTimer;
+    public Image clockbase;
+    public Image clock;
+    public Text text;
+
+
+    void Start()
+    {
+        clockbase.enabled = false;
+        clock.enabled = false;
+        text.enabled = false;
+        bucketStates = bucket.GetComponent<BucketStates>();
+        rb = bucket.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (bucket.transform.position.y < sea.transform.position.y)
+        {
+            bucket.SetActive(false);
+            bucket.transform.parent = null;
+            timer = initialTimer;
+            StartCoroutine(DelayRespawn());
+        }
+    }
+
+    IEnumerator DelayRespawn()
+    {
+        yield return new WaitForSeconds(1f);
+
+        rb.velocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        RespawnTimer();
+    }
+
+    void RespawnTimer()
+    {
+        clockbase.enabled = true;
+        clock.enabled = true;
+        text.enabled = true;
+
+        timer -= Time.deltaTime;
+        text.text = timer.ToString("F0");
+        clock.fillAmount = timer / initialTimer;
+
+        if (timer <= 0)
+        {
+            clockbase.enabled = false;
+            clock.enabled = false;
+            text.enabled = false;
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        bucketStates.currentState = BucketStates.BucketState.Dropped;
+        bucket.transform.position = spawnPoint.transform.position;
+        bucket.transform.rotation = spawnPoint.transform.rotation;
+        bucket.SetActive(true);
+    }
+}
---
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/UI/TimerBars/RespawnerTorch.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RespawnerTorch : MonoBehaviour
+{
+    public GameObject torch;
+    TorchStates torchStates;
+    public GameObject spawnPoint;
+    public GameObject sea;
+    private Rigidbody rb;
+
+    public float timer;
+    public float initialTimer;
+    public Image clockbase;
+    public Image clock;
+    public Text text;
+
+
+    void Start()
+    {
+        clockbase.enabled = false;
+        clock.enabled = false;
+        text.enabled = false;
+        torchStates = torch.GetComponent<TorchStates>();
+        rb = torch.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (torch.transform.position.y < sea.transform.position.y)
+        {
+            torch.SetActive(false);
+            torch.transform.parent = null;
+            timer = initialTimer;
+            StartCoroutine(DelayRespawn());
+        }
+    }
+
+    IEnumerator DelayRespawn()
+    {
+        yield return new WaitForSeconds(1f);
+
+        rb.velocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        RespawnTimer();
+    }
+
+    void RespawnTimer()
+    {
+        clockbase.enabled = true;
+        clock.enabled = true;
+        text.enabled = true;
+
+        timer -= Time.deltaTime;
+        text.text = timer.ToString("F0");
+        clock.fillAmount = timer / initialTimer;
+
+        if (timer <= 0)
+        {
+            clockbase.enabled = false;
+            clock.enabled = false;
+            text.enabled = false;
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        torchStates.currentState = TorchStates.TorchState.Dropped;
+        torch.transform.position = spawnPoint.transform.position;
+        torch.transform.rotation = spawnPoint.transform.rotation;
+        torch.SetActive(true);
+    }
+}

9568fe46f96cdb87fe490b10bf7f3d3a6a4f786b
Tom <tom.gibbs@hotmail.co.uk>
2019-05-08 20:01:44 +0100

    Controller support for all scene transitions added
    Co-Authored-By: Henry Crofts <hwcrofts@gmail.com>

--- ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/IntroductionScenes/PlayerJoinTutorial.cs
+++ ShipHappens_UnityBuild/ShipHappens/Assets/Scripts/IntroductionScenes/PlayerJoinTutorial.cs
@@ -34,7 +34,7 @@ public class PlayerJoinTutorial : MonoBehaviour
 
         TogglePlayerUI();
 
-        if (Input.GetKeyDown("joystick 1 button 7"))
+        if ((Input.GetKeyDown("joystick 1 button 7")) || (Input.GetKeyDown("joystick 2 button 7")) || (Input.GetKeyDown("joystick 3 button 7")) || (Input.GetKeyDown("joystick 4 button 7")))
         {
             levelManagerScript.FadeToLevel(2);
         }
